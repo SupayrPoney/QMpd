627e02c685c24be5047bf4fcf3bdcd058cfa6d67
brochape <brochape@ulb.ac.be>
2014-12-23 01:16:24 +0100

    [add] Added the widget for the different buttons (not in use yet)

---
+++ ButtonsWidget.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# -*- coding: utf8 -*-
+
+
+
+import sys
+from PyQt4 import *
+
+class ButtonsWidget(QtGui.QWidget):
+    def __init__(self,parent = None):
+        QtGui.QWidget.__init__(self)
